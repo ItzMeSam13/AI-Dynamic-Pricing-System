18f9ce22f585723fcade27aeabcbe297e6010236
ItzMeSam <sampreetkalkundrikar8@gmail.com>
2025-03-27 15:26:23 +0530

    fetching setup done

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import AuthRoutes from './routes/AuthRoutes.js';
-
+import { fetchProducts } from './controllers/fetchProducts.js';
+import { protectRoute } from './middleware/Authmiddleware.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
     res.send("welcome to API");
 });
 app.use("/auth", AuthRoutes);
+app.use("/dashboard",protectRoute,fetchProducts);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
